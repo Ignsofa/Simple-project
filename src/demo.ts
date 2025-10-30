9613b7523212f7e2cc5ba1fce9eac0f9567c8a21
ignsofa <ignsofa@gmail.com>
2025-10-30 17:03:34 +0200

    feat: add basic types for utils (number/string)

--- src/demo.ts
+++ src/demo.ts
@@ -1,4 +1,4 @@
-import { add, capitalize } from './index.js'; // у ESM обов'язково .js
+import { add, capitalize } from './index.js';
 
-console.log('sum(any):', add(2, 3));
-console.log('capitalize(any):', capitalize('hello'));
+console.log('sum(typed):', add(2, 3));
+console.log('capitalize(typed):', capitalize('hello'));
